a7a4a5b729c7956f539e6c546f824b64888072a5
younhoso <thdbsgh3443@naver.com>
2018-09-09 00:48:12 +0900

    9/9(일) 12:47 소윤호 Register 수정
    9/9(일) 12:47 소윤호 Register 수정

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-function calObj(mode) {
-	var obj = {
-		"idChecker": function ($el, email, password) {
-			if ($el.val() === "") {
-				$el.addClass('shake').stop().delay(600).queue(function () {
-					$(this).removeClass('shake');
-				});
-				if ($el.val().length > 1) {
-					$el.removeClass('shake');
-				}
-			} else {
-				$.post($("#myForm").attr("action"),
-    					$("#myForm:input").serializeArray(),
-    					function (data) {
-    						$("div#ack").html(data);
-    					});
-			}
-		}
-	}
-	return obj[mode];
-}
+
 
 
